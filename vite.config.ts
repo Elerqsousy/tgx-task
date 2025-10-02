c2f4dce58186521dabb57794d5a60c6565b506e7
Mahmoud Abdulhaleem <m_rizk@genor.com>
2025-10-02 14:45:45 +0300

    change vite configuration and github workflows to deploy on netlify

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,8 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: '/tgx-task/', // Base URL for GitHub Pages
   build: {
-    outDir: 'dist', // Default output directory
+    outDir: 'dist',
   },
   resolve: {
     alias: {
